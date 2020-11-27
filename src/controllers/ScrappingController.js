14a10ba4af57e7fc60a147997a2af84be632d9af
João Paulo M <joaopaulopmedeiros@gmail.com>
2020-11-27 14:52:02 -0300

    Controllers - rendering views and showing images

---
+++ src/controllers/ScrappingController.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+module.exports = {
+    index(req, res) {
+        return res.render("index.html");
+    },
+
+    async search(req, res) {
+
+        try {
+            const { url } = req.body;
+
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+
+            await page.goto(url);
+            
+            const path = 'public/images/example.png';
+
+            await page.screenshot({path});
+          
+            await browser.close();
+            
+            return res.redirect(`/example.png`);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
